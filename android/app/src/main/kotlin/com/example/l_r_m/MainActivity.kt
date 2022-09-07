7378ce1342ff469a974c43cd986b21cdfc93df95
Akana carlewis <acarlewis@gmail.com>
2022-09-07 23:50:31 +0300

    stage

---
+++ android/app/src/main/kotlin/com/example/l_r_m/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.l_r_m
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Land Of Restoration Ministry',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  late WebViewController _webViewController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: WillPopScope(
+          onWillPop: () async {
+            if(await _webViewController.canGoBack()){
+              _webViewController.goBack();
+              return false;
+            }else{
+              return true;
+            }
+          },
+          child: WebView(
+            initialUrl: "http://lrm.rf.gd/",
+            javascriptMode: JavascriptMode.unrestricted,
+            onWebViewCreated: (WebViewController controller){
+              _webViewController = controller;
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
